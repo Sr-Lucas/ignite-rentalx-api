84376ad50728df37486b846ee409643a467f5778
Sr-Lucas <lucastsantos.developer@gmail.com>
2021-09-26 19:37:01 -0300

    feat: remove files after ImportCategory is done

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -26,6 +26,7 @@ export class ImportCategoryUseCase {
           categories.push({ name, description });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
